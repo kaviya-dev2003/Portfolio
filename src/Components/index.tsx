8c977f0e9dbbf0351b69d1e954b67ff00cff7994
KaviyaVenkatesh <frontend.nulinz@gmail.com>
2026-01-10 12:40:36 +0530

    Fix case sensitivity: Remove Duplicate Uppercase Components folder

--- src/Components/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as Navbar } from './Navbar/index';
-export { default as Footer } from './Footer/index';
-export { default as Button } from './Button/index';
-export { default as Toast } from './Toast/index';
