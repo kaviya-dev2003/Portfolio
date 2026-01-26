497af0e1015e058842a7045f784b052e7925710d
KaviyaVenkatesh <frontend.nulinz@gmail.com>
2026-01-26 12:34:35 +0530

    c

--- client/src/pages/Contact/index.tsx
+++ client/src/pages/Contact/index.tsx
@@ -194,7 +194,7 @@ const Contact: React.FC = () => {
   return (
     <Container>
       <Header variants={slideUp} initial="initial" animate="animate">
-        <Title>Get In Touch <span style={{ fontSize: '10px', opacity: 0.3 }}>v2.0-FINAL</span></Title>
+        <Title>Get In Touch</Title>
         <p>Interested in collaborating or have a question? Leave a message below.</p>
       </Header>
 
